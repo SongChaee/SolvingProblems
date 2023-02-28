eecd94e931ca90e24271529d4708fa91fa50e5e7
SongChaee <122426072+SongChaee@users.noreply.github.com>
2023-02-28 14:25:31 +0900

    [D4] Title: [S/W 문제해결 기본] 9일차 - 중위순회, Time: 159 ms, Memory: 22,852 KB -BaekjoonHub

--- SWEA/D4/1231. ［S／W 문제해결 기본］ 9일차 － 중위순회/［S／W 문제해결 기본］ 9일차 － 중위순회.java
+++ SWEA/D4/1231. ［S／W 문제해결 기본］ 9일차 － 중위순회/［S／W 문제해결 기본］ 9일차 － 중위순회.java
@@ -1,12 +1,13 @@
 import java.util.Scanner;
 
 public class Solution {
+    static int N;
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int T = 10;
 		
 		for(int tc = 1; tc <= T; tc++) {
-			int N = Integer.parseInt(sc.nextLine());
+			N = Integer.parseInt(sc.nextLine());
 			int log = (int) (Math.log(N) / Math.log(2));
 			char[] arr = new char[(int) Math.pow(2, log + 1)];
 			
@@ -23,7 +24,7 @@ public class Solution {
 	}
 	
 	static void inorderTraverse(int i, char[] arr) {
-		if(i <= arr.length - 1) {
+		if(i <= N) {
 			inorderTraverse(i * 2, arr);
 			if(arr[i] != '\u0000')
 				System.out.print(arr[i]);
