48a7a3123a1866e944e5e53dab44915275f2528f
SongChaee <122426072+SongChaee@users.noreply.github.com>
2023-03-20 12:13:20 +0900

    [Gold V] Title: 별 찍기 - 10, Time: 348 ms, Memory: 48016 KB -BaekjoonHub

--- 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.java
+++ 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.java
@@ -8,6 +8,7 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
 		pattern = new char[N][N];
+		
 		for(int i = 0; i < N; i++)
 			for(int j = 0; j < N; j++)
 				pattern[i][j] = ' ';
@@ -25,25 +26,16 @@ public class Main {
 	}
 	
 	static void recur(int startx, int starty, int size) {
-		if(size == 3) {
-			for(int i = startx; i < startx + size; i++) {
-				for(int j = starty; j < starty + size; j++) {
-					if(i == startx + 1 && j == starty + 1)
-						pattern[i][j] = ' ';
-					else
-						pattern[i][j] = '*';
-				}
-			}
-			return;	
+		if(size == 1) {
+			pattern[startx][starty] = '*';
+			return;
 		}
 		
-		recur(startx, starty, size/3);
-		recur(startx, starty + size/3, size/3);
-		recur(startx, starty + size/3*2, size/3);
-		recur(startx + size/3, starty, size/3);
-		recur(startx + size/3, starty + size/3*2, size/3);
-		recur(startx + size/3*2, starty, size/3);
-		recur(startx + size/3*2, starty + size/3, size/3);
-		recur(startx + size/3*2, starty + size/3*2, size/3);
+		for(int i = 0; i < 3; i++)
+			for(int j = 0; j < 3; j++) {
+				if(i == 1 && j == 1)
+					continue;
+				recur(startx + size/3*i, starty + size/3*j, size/3);
+			}
 	}
 }
