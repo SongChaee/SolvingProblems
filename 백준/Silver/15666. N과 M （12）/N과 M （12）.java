ae98a60f4d830c697ad40d6b8adad03e3f9fc40c
SongChaee <122426072+SongChaee@users.noreply.github.com>
2024-01-23 16:45:38 +0900

    [Silver II] Title: N과 M (12), Time: 136 ms, Memory: 14852 KB -BaekjoonHub

---
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.java
@@ -0,0 +1,48 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static int N, seqLen;
+	public static int[] given;
+	public static boolean[] used;
+	public static StringBuilder sb = new StringBuilder();
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		N = Integer.parseInt(st.nextToken());
+		seqLen = Integer.parseInt(st.nextToken());
+		
+		given = new int[N];
+		used = new boolean[N];
+		st = new StringTokenizer(br.readLine(), " ");
+		for(int i = 0; i < N; i++)
+			given[i] = Integer.parseInt(st.nextToken());
+		Arrays.sort(given);
+		
+		int[] arr = new int[seqLen];
+		seq(0, given[0], arr);
+		
+		System.out.print(sb.toString());
+	}
+	
+	public static void seq(int idx, int num, int[] arr) {
+		if(idx == seqLen) {
+			for(int i = 0; i < seqLen; i++)
+				sb.append(arr[i]).append(" ");
+			sb.append("\n");
+			return;
+		}
+		
+		int temp = 0;
+		for(int i = 0; i < N; i++) {
+			if(temp != given[i] && num <= given[i]) {
+				arr[idx] = given[i];
+				temp = given[i];
+				seq(idx + 1, given[i], arr);
+			}
+		}
+	}
+}
