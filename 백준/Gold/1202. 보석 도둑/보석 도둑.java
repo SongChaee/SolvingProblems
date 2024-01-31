6215c8cda415d3ba214a7a8e57633b456bba7f7b
SongChaee <sce9842@gmail.com>
2024-02-01 00:26:02 +0900

    [Gold II] Title: 보석 도둑, Time: 1988 ms, Memory: 137724 KB -BaekjoonHub

--- 백준/Gold/1202. 보석 도둑/보석 도둑.java
+++ 백준/Gold/1202. 보석 도둑/보석 도둑.java
@@ -1,80 +1,64 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
- 
-class Jewelry {
-    int mass; // 무게
-    int value; // 가격
- 
-    Jewelry(int mass, int value) {
-        this.mass = mass;
-        this.value = value;
-    }
-}
- 
+
 public class Main {
- 
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int K = Integer.parseInt(st.nextToken());
- 
-        Jewelry[] jewelries = new Jewelry[N];
-        for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine());
-            int m = Integer.parseInt(st.nextToken());
-            int v = Integer.parseInt(st.nextToken());
- 
-            jewelries[i] = new Jewelry(m, v);
-        }
-        // 무게를 오름차순 정렬하되, 무게가 같을 경우 가격을 내림차순 정렬.
-        Arrays.sort(jewelries, new Comparator<Jewelry>() {
- 
-            @Override
-            public int compare(Jewelry o1, Jewelry o2) {
-                if (o1.mass == o2.mass) {
-                    return o2.value - o1.value;
-                }
-                return o1.mass - o2.mass;
-            }
- 
-        });
- 
-        int[] bags = new int[K];
-        for (int i = 0; i < K; i++) {
-            bags[i] = Integer.parseInt(br.readLine());
-        }
-        // 가방의 무게를 오름차순 정렬
-        Arrays.sort(bags);
- 
-        // 우선순위 큐는 항상 가격이 내림차순 정렬되도록 설정.
-        PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
-        long ans = 0;
-        for (int i = 0, j = 0; i < K; i++) {
-            // 현재 가방의 무게보다 작거나 같은 보석을 모두 우선순위 큐에 넣음.
-            while (j < N && jewelries[j].mass <= bags[i]) {
-                pq.offer(jewelries[j++].value);
-            }
- 
-            // 우선순위 큐에 있는 요소를 하나 빼서 가방에 넣음.
-            // 이 때, 우선순위 큐는 내림차순 정렬이 되어있으므로
-            // 첫 번째 요소는 가장 큰 가격을 의미함.
-            if (!pq.isEmpty()) {
-                ans += pq.poll();
-            }
-        }
- 
-        bw.write(ans + "\n");
-        bw.flush();
-        bw.close();
-        br.close();
-    }
- 
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		int JewelN = Integer.parseInt(st.nextToken());
+		int BagN = Integer.parseInt(st.nextToken());
+		
+		Jewel[] jewels = new Jewel[JewelN];
+		for(int n = 0; n < JewelN; n++) {
+			st = new StringTokenizer(br.readLine(), " ");
+			int w = Integer.parseInt(st.nextToken());
+			int v = Integer.parseInt(st.nextToken());
+			jewels[n] = new Jewel(w, v);
+		}
+		Arrays.sort(jewels);
+		
+		int[] bags = new int[BagN];
+		for(int n = 0; n < BagN; n++) {
+			bags[n] = Integer.parseInt(br.readLine());
+		}
+		Arrays.sort(bags);
+		
+		long value = 0;
+		PriorityQueue<Integer> q = new PriorityQueue<>(Comparator.reverseOrder());
+		int j = 0;
+		for(int b = 0; b < BagN; b++) {
+			while(j < JewelN && jewels[j].weight <= bags[b]) {
+				q.add(jewels[j].value);
+				j++;
+			}
+			
+			if(!q.isEmpty()) {
+				value += q.poll();
+			}
+		}
+		
+		System.out.print(value);
+	}
+
+}
+
+class Jewel implements Comparable<Jewel>{
+	int weight;
+	int value;
+	
+	public Jewel(int weight, int value) {
+		this.weight = weight;
+		this.value = value;
+	}
+
+	@Override
+	public int compareTo(Jewel j) {
+		if(weight == j.weight)
+			return j.value - value;
+		return weight - j.weight;
+	}
 }
