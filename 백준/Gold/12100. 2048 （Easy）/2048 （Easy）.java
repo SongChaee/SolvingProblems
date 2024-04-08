c80a4f2ab397443c01dba2da87e53bee2fe9d301
SongChaee <122426072+SongChaee@users.noreply.github.com>
2024-04-09 01:33:51 +0900

    [Gold II] Title: 2048 (Easy), Time: 288 ms, Memory: 24992 KB -BaekjoonHub

--- 백준/Gold/12100. 2048 （Easy）/2048 （Easy）.java
+++ 백준/Gold/12100. 2048 （Easy）/2048 （Easy）.java
@@ -5,165 +5,135 @@ import java.util.StringTokenizer;
 
 public class Main {
 	public static int N;
-	public static int[][] board, boardtemp;
-	public static int max;
-	
+	public static int[] dx = { -1, 0, 1, 0 };
+	public static int[] dy = { 0, 1, 0, -1 };
+	public static int ans = 0;
+
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		N = Integer.parseInt(br.readLine());
-		board = new int[N][N];
-		boardtemp = new int[N][N];
-		max = 2;
-		
-		// 보드 초기 상태 입력
-		int notzerocnt = 0;
-		int onlyi = 0;
-		int onlyj = 0;
 		StringTokenizer st;
-		for(int i = 0; i < N; i++) {
+
+		N = Integer.parseInt(br.readLine());
+		int[][] board = new int[N][N];
+
+		for (int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine(), " ");
-			for(int j = 0; j < N; j++) {
+			for (int j = 0; j < N; j++)
 				board[i][j] = Integer.parseInt(st.nextToken());
-				if(board[i][j] != 0) {
-					notzerocnt++;
-					onlyi = i;
-					onlyj = j;
-				}
-			}
 		}
-		
-		if(notzerocnt == 1) {
-			System.out.print(board[onlyi][onlyj]);
+
+		move(board, 0);
+
+		System.out.print(ans);
+
+	}
+
+	public static void move(int[][] board, int cnt) {
+		if (cnt >= 5) {
+			int max = 0;
+			for (int i = 0; i < N; i++)
+				for (int j = 0; j < N; j++)
+					max = Math.max(max, board[i][j]);
+			ans = Math.max(ans, max);
 			return;
 		}
-		
-		for(int i = 1; i < 5; i++)
-			move(board, 0, i);
-		
-		System.out.print(max);
+
+		for (int dir = 0; dir < 4; dir++) {
+			int[][] next = push(copy(board), dir);
+			move(next, cnt + 1);
+		}
+	}
+
+	public static int[][] push(int[][] board, int dir) {
+		boolean[][] merged = new boolean[N][N];
+
+		switch (dir) {
+		case (0):
+			for (int j = 0; j < N; j++) {
+				for (int i = 1; i < N; i++) {
+					if (board[i][j] == 0)
+						continue;
+					int x = i;
+					while (x - 1 >= 0 && board[x - 1][j] == 0) {
+						board[x - 1][j] = board[x][j];
+						board[x][j] = 0;
+						x--;
+					}
+					if (x - 1 >= 0 && board[x - 1][j] == board[x][j] && !merged[x - 1][j]) {
+						board[x - 1][j] *= 2;
+						board[x][j] = 0;
+						merged[x - 1][j] = true;
+					}
+				}
+			}
+			break;
+		case (1):
+			for (int i = 0; i < N; i++) {
+				for (int j = N - 2; j >= 0; j--) {
+					if (board[i][j] == 0)
+						continue;
+					int y = j;
+					while (y + 1 < N && board[i][y + 1] == 0) {
+						board[i][y + 1] = board[i][y];
+						board[i][y] = 0;
+						y++;
+					}
+					if (y + 1 < N && board[i][y + 1] == board[i][y] && !merged[i][y + 1]) {
+						board[i][y + 1] *= 2;
+						board[i][y] = 0;
+						merged[i][y + 1] = true;
+					}
+				}
+			}
+			break;
+		case (2):
+			for (int j = 0; j < N; j++) {
+				for (int i = N - 2; i >= 0; i--) {
+					if (board[i][j] == 0)
+						continue;
+					int x = i;
+					while (x + 1 < N && board[x + 1][j] == 0) {
+						board[x + 1][j] = board[x][j];
+						board[x][j] = 0;
+						x++;
+					}
+					if (x + 1 < N && board[x + 1][j] == board[x][j] && !merged[x + 1][j]) {
+						board[x + 1][j] *= 2;
+						board[x][j] = 0;
+						merged[x + 1][j] = true;
+					}
+				}
+			}
+			break;
+		case (3):
+			for (int i = 0; i < N; i++) {
+				for (int j = 1; j < N; j++) {
+					if (board[i][j] == 0)
+						continue;
+					int y = j;
+					while (y - 1 >= 0 && board[i][y - 1] == 0) {
+						board[i][y - 1] = board[i][y];
+						board[i][y] = 0;
+						y--;
+					}
+					if (y - 1 >= 0 && board[i][y - 1] == board[i][y] && !merged[i][y - 1]) {
+						board[i][y - 1] *= 2;
+						board[i][y] = 0;
+						merged[i][y - 1] = true;
+					}
+				}
+			}
+			break;
+		}
+
+		return board;
 	}
 	
-	// 보드 안의 숫자 이동
-	// dir : 1(위) 2(아래) 3(왼쪽) 4(오른쪽)
-	public static void move(int[][] arr, int num, int dir) {
-		// 종료 조건
-		// 숫자가 한개 남았을 때 -> 아직 구현 미완성
-		if (num == 5) {
-			int arrmax = 0;
-			for(int i = 0; i < N; i++)
-				for(int j = 0; j < N; j++)
-					arrmax = Math.max(arrmax, arr[i][j]);
-			max = Math.max(max, arrmax);
-			return;
-		}
-		
-        // 배열 복사
-		int[][] temp = new int[N][N];
-		for(int i = 0; i < N; i++)
-			for(int j = 0; j < N; j++)
-				temp[i][j] = arr[i][j];
-		
-		gravity(temp, dir);
-		attach(temp, dir);
-		gravity(temp, dir);
-		
-		for(int i = 1; i < 5; i++)
-			move(temp, num + 1, i);
-	}
-	
-	// 특정 방향으로 모든 숫자 이동
-	public static void gravity(int[][] arr, int dir) {
-		if(dir == 1) {
-			for(int j = 0; j < N; j++) {
-				for(int i = 0; i < N; i++) {
-					if(arr[i][j] == 0) {
-						int ni = i;
-						while(ni < N-1 && arr[ni][j] == 0) ni++;
-						arr[i][j] = arr[ni][j];
-						arr[ni][j] = 0;
-					}
-				}
-			}
-		}
-		else if(dir == 2) {
-			for(int j = 0; j < N; j++) {
-				for(int i = N-1; i >= 0; i--) {
-					if(arr[i][j] == 0) {
-						int ni = i;
-						while(ni > 0 && arr[ni][j] == 0) ni--;
-						arr[i][j] = arr[ni][j];
-						arr[ni][j] = 0;
-					}
-				}
-			}
-		}
-		else if(dir == 3) {
-			for(int i = 0; i < N; i++) {
-				for(int j = 0; j < N; j++) {
-					if(arr[i][j] == 0) {
-						int nj = j;
-						while(nj < N-1 && arr[i][nj] == 0) nj++;
-						arr[i][j] = arr[i][nj];
-						arr[i][nj] = 0;
-					}
-				}
-			}
-		}
-		else {
-			for(int i = 0; i < N; i++) {
-				for(int j = N-1; j >= 0; j--) {
-					if(arr[i][j] == 0) {
-						int nj = j;
-						while(nj > 0 && arr[i][nj] == 0) nj--;
-						arr[i][j] = arr[i][nj];
-						arr[i][nj] = 0;
-					}
-				}
-			}
-		}
-	}
-	
-	// 연속으로 같은 숫자가 있을 때 합치기
-	public static void attach(int[][] arr, int dir) {
-		if(dir == 1) {
-			for(int j = 0; j < N; j++) {
-				for(int i = 0; i < N-1; i++) {
-					if(arr[i][j] != 0 && arr[i][j] == arr[i+1][j]) {
-						arr[i][j] *= 2;
-						arr[i+1][j] = 0;
-					}
-				}
-			}
-		}
-		else if(dir == 2) {
-			for(int j = 0; j < N; j++) {
-				for(int i = N-1; i > 0; i--) {
-					if(arr[i][j] != 0 && arr[i][j] == arr[i-1][j]) {
-						arr[i][j] *= 2;
-						arr[i-1][j] = 0;
-					}
-				}
-			}
-		}
-		else if(dir == 3) {
-			for(int i = 0; i < N; i++) {
-				for(int j = 0; j < N-1; j++) {
-					if(arr[i][j] != 0 && arr[i][j] == arr[i][j+1]) {
-						arr[i][j] *= 2;
-						arr[i][j+1] = 0;
-					}
-				}
-			}
-		}
-		else {
-			for(int i = 0; i < N; i++) {
-				for(int j = N-1; j > 0; j--) {
-					if(arr[i][j] != 0 && arr[i][j] == arr[i][j-1]) {
-						arr[i][j] *= 2;
-						arr[i][j-1] = 0;
-					}
-				}
-			}
-		}
-	}
+	public static int[][] copy(int[][] origin) {
+        int[][] copy = new int[N][N];
+        for (int i = 0; i < N; i++) 
+            copy[i] = origin[i].clone();
+        
+        return copy;
+    }
 }
