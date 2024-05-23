355e37d9991a09a38774b618445cc2e2861591fe
SongChaee <122426072+SongChaee@users.noreply.github.com>
2024-05-23 23:16:34 +0900

    [Silver III] Title: 계단 오르기, Time: 124 ms, Memory: 14176 KB -BaekjoonHub

---
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.io.IOException;
+
+public class Main {
+
+	static int dp[];
+	static int arr[];
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int N = Integer.parseInt(br.readLine());
+
+		dp = new int[N + 1];
+		arr = new int[N + 1];
+
+		for (int i = 1; i <= N; i++)
+			arr[i] = Integer.parseInt(br.readLine());
+
+		Arrays.fill(dp, -1);
+		dp[0] = arr[0];
+		dp[1] = arr[1];
+
+		if (N >= 2)
+			dp[2] = arr[1] + arr[2];
+
+		System.out.println(find(N));
+
+	}
+
+	static int find(int N) {
+		if (dp[N] == -1) {
+			dp[N] = Math.max(find(N - 2), find(N - 3) + arr[N - 1]) + arr[N];
+		}
+
+		return dp[N];
+	}
+
+}
