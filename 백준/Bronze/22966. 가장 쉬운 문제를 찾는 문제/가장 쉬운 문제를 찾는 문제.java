d0706d174d4895d72e3d6501fdab4a624244610f
SongChaee <122426072+SongChaee@users.noreply.github.com>
2024-06-03 23:03:00 +0900

    [Bronze II] Title: 가장 쉬운 문제를 찾는 문제, Time: 128 ms, Memory: 14156 KB -BaekjoonHub

---
+++ 백준/Bronze/22966. 가장 쉬운 문제를 찾는 문제/가장 쉬운 문제를 찾는 문제.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+
+		int size = Integer.parseInt(br.readLine());
+		
+		int min = 5;
+		String answer = "";
+        
+		while (size-- > 0) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+            
+			String s = st.nextToken();
+			int n = Integer.parseInt(st.nextToken());
+			
+			if(n < min) {
+				min = n;
+				answer = s;
+			}
+		}
+        
+		System.out.print(answer);
+	}
+}
